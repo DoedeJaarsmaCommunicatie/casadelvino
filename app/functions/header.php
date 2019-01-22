8a2621813be2a4b34396ca7bdcd3454e66cba1e0
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-01-22 10:39:23 +0100

    added search functions
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

---
+++ app/functions/header.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mitch
+ * Date: 2019-01-22
+ * Time: 10:36
+ */
+
+function the_search_bar()
+{
+    return \Timber\Timber::compile('templates/functions/header/search.twig');
+}
--- functions.php
+++ functions.php
@@ -5,3 +5,16 @@
  * Date: 2019-01-21
  * Time: 10:27
  */
+add_theme_support('woocommerce');
+
+array_map(
+    function ($file) {
+        $file = get_stylesheet_directory() . "/app/{$file}.php";
+        load_template($file);
+    },
+    [
+        'init',
+        
+        'functions/header',
+    ]
+);

e470bf160279997a90b89eb19e639864bd868ccb
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-01-22 14:39:15 +0100

    product card start
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -6,16 +6,16 @@
  * Time: 11:07
  */
 
-if( class_exists( 'Timber' ) ) {
-	$context                = Timber::get_context();
-	
-	$context['post']        = Timber::get_post($GLOBALS['post']->ID);
-	$product                = wc_get_product( $context['post']->ID );
-	$context['product']     = $product;
+if (class_exists('Timber')) {
+    $context                = Timber::get_context();
+    
+    $context['post']        = Timber::get_post($GLOBALS['post']->ID);
+    $product                = wc_get_product($context['post']->ID);
+    $context['product']     = $product;
 
-	if( is_front_page() ) {
-		Timber::render('templates/woocommerce/partials/tease-product.twig', $context);
-	} else {
-		Timber::render('templates/woocommerce/partials/tease-archive.twig', $context);
-	}
+    if (is_front_page()) {
+        Timber::render('templates/woocommerce/partials/tease-product.twig', $context);
+    } else {
+        Timber::render('templates/woocommerce/partials/tease-archive.twig', $context);
+    }
 }

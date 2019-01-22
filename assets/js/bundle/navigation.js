e099419ac6256dcd0a43e75cda48d6f393b31ea5
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-01-22 13:06:25 +0100

    submenu
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- assets/js/bundle/navigation.js
+++ assets/js/bundle/navigation.js
@@ -1,3 +1,11 @@
 document.addEventListener('DOMContentloaded', () => {
+	document.querySelectorAll('.parent-nav-item').forEach( el => {
+		el.addEventListener('mouseover', () => {
+			el.querySelector('.drop-down').classList.add('active')
+		});
 
+		el.addEventListener('mouseout', () => {
+			el.querySelector('.drop-down').classList.remove('active')
+		})
+	})
 });

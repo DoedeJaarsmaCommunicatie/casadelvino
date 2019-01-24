15134554ac32dbfc025b0db7807238f18f06c622
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-01-24 15:12:59 +0100

    render relateds
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -12,10 +12,10 @@ if (class_exists('Timber')) {
     $context['post']        = Timber::get_post($GLOBALS['post']->ID);
     $product                = wc_get_product($context['post']->ID);
     $context['product']     = $product;
-	
-	$related_limit               =  2;
-	$related_ids                 =  wc_get_related_products($context['product']->get_id(), $related_limit);
-	$context['related_products'] =  Timber::get_posts($related_ids);
+    
+    $related_limit               =  2;
+    $related_ids                 =  wc_get_related_products($context['product']->get_id(), $related_limit);
+    $context['related_products'] =  Timber::get_posts($related_ids);
 
     Timber::render('templates/woocommerce/partials/tease-product.twig', $context);
 //    if (is_front_page()) {

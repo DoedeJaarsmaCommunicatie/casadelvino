d82767f95ca021cef02f7f3b4fc4e63a5db384b9
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-01-24 15:22:29 +0100

    setting product
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -15,8 +15,9 @@ if (class_exists('Timber')) {
     
     $related_limit               =  2;
     $related_ids                 =  wc_get_related_products($context['product']->get_id(), $related_limit);
-    $context['related_products'] =  wc_get_product($related_ids);
-
+    $context['related_products'] =  Timber::get_posts($related_ids);
+	wp_reset_postdata();
+	
     Timber::render('templates/woocommerce/partials/tease-product.twig', $context);
 //    if (is_front_page()) {
 //    } else {

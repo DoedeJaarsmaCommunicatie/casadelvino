c32707779340d1af0334038ff100de603dac2060
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-01-24 07:00:37 +0100

    added axios dependency
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- assets/vue/app.js
+++ assets/vue/app.js
@@ -1,6 +1,10 @@
 window.Vue = require('vue');
+const axios = require('axios');
 
 Vue.component('add-to-cart', require('./components/addToCart').default);
+
+Vue.prototype.$http = axios;
+Vue.prototype.$http.defaults.baseURL = 'https://casadelvino.nl';
 
 window.addEventListener('load', ()  => {
 	const app = new Vue({

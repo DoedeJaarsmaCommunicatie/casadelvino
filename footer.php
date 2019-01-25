2ec347ba0703d616e297afb1545861e00d39854e
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-01-25 14:13:57 +0100

    adding files for elementor
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

---
+++ footer.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mitch
+ * Date: 2018-12-03
+ * Time: 08:45
+ */
+
+$timberContext = $GLOBALS['timberContext']; // @codingStandardsIgnoreFile
+if (! isset($timberContext)) {
+    throw new \Exception('Timber context not set in footer.');
+}
+$timberContext['content'] = ob_get_contents();
+ob_end_clean();
+$templates = array( 'templates/layouts/base.twig' );
+Timber::render($templates, $timberContext);
---
+++ header.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mitch
+ * Date: 2018-12-03
+ * Time: 08:43
+ */
+
+$GLOBALS['timberContext'] = Timber::get_context();
+ob_start();

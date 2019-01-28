af1b4cc45ce202e476cf2a07c598a021794b77a2
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-01-28 13:13:20 +0100

    added image to related products
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- app/loaders/locations.php
+++ app/loaders/locations.php
@@ -17,5 +17,13 @@ function cdv_register_elementor_locations($elementor_theme_manager)
             'edit_in_content' => true,
         ]
     );
+    $elementor_theme_manager->register_location(
+        'product-related-image',
+        [
+            'label' => __('Afbeelding gerelateerd product', 'casadelvino'),
+            'multiple' => false,
+            'edit_in_content' => true,
+        ]
+    );
 }
 add_action('elementor/theme/register_locations', 'cdv_register_elementor_locations');

09c3a26ef484fa582e16487a0b91e660d2604d4e
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-01-29 09:15:39 +0100

    register widget location
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- app/widgets/registers.php
+++ app/widgets/registers.php
@@ -1,3 +1,13 @@
 <?php
 
-register_widget('product_filter_widget');
+function register_shop_widgets()
+{
+    register_sidebar([
+        'name'  => __('Filter sidebar', 'casadelvino'),
+        'id'    => 'product_filter_widget',
+        'before_widget' => '<div>',
+        'after_widget'  => '</div>',
+    ]);
+}
+
+add_action('widgets_init', 'register_shop_widgets');

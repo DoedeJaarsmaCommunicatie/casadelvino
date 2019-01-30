9dad33fb2a653b0826b9d2e50d38dc858357f181
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-01-30 09:29:02 +0100

    unhook breadcrumb woocommerce
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

---
+++ app/hooks/dehook.php
@@ -0,0 +1,3 @@
+<?php
+
+remove_action('woocommerce_before_main_content', 'woocommerce_breadcrumb', 20, 0);
--- functions.php
+++ functions.php
@@ -20,6 +20,8 @@ array_map(
         'loaders/enqueue',
         'loaders/locations',
         'loaders/customizer',
+	    
+        'hooks/dehook',
 
         'widgets/registers',
         

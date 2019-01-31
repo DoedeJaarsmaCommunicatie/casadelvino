d636f87097ad25bfe23bf64425ceb3a5f87e1d19
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-01-31 12:37:49 +0100

    added new location for menu
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- app/init.php
+++ app/init.php
@@ -1,1 +1,11 @@
 <?php
+
+add_filter( 'timber/context', 'add_to_context' );
+
+function add_to_context( $context ) {
+	// So here you are adding data to Timber's context object, i.e...
+	// Now, in similar fashion, you add a Timber Menu and send it along to the context.
+	$context['mobileSubmenu'] = new \Timber\Menu( 'mobile-sub-menu' );
+	
+	return $context;
+}

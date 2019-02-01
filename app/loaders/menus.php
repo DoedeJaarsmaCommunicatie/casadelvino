b7a83432867928889a98a9fbfa0990337fb25e2a
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-02-01 14:15:10 +0100

    primaryMenu
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- app/loaders/menus.php
+++ app/loaders/menus.php
@@ -5,7 +5,7 @@ function add_to_context($context)
 {
     // So here you are adding data to Timber's context object, i.e...
     // Now, in similar fashion, you add a Timber Menu and send it along to the context.
-    $context['menu'] = new \Timber\Menu('primary-menu');
+    $context['primaryMenu'] = new \Timber\Menu('primary-menu');
     $context['mobileSubmenu'] = new \Timber\Menu('mobile-sub-menu');
     
     return $context;

17017f20617c5bb1509890b6bc4405a62f3c1dfc
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-02-01 13:41:21 +0100

    added normal menu as menu
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- app/loaders/menus.php
+++ app/loaders/menus.php
@@ -3,11 +3,12 @@
 add_filter('timber/context', 'add_to_context');
 function add_to_context($context)
 {
-	// So here you are adding data to Timber's context object, i.e...
-	// Now, in similar fashion, you add a Timber Menu and send it along to the context.
-	$context['mobileSubmenu'] = new \Timber\Menu('mobile-sub-menu');
-	
-	return $context;
+    // So here you are adding data to Timber's context object, i.e...
+    // Now, in similar fashion, you add a Timber Menu and send it along to the context.
+    $context['mobileSubmenu'] = new \Timber\Menu('mobile-sub-menu');
+    $context['menu'] = new \Timber\Menu('primary-menu');
+    
+    return $context;
 }
 
 function cdv_register_nav_menu()

983315a4409e19e699203c6e7ce0f4f7a947f485
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-02-01 12:47:55 +0100

    mobile submenu
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- app/init.php
+++ app/init.php
@@ -13,14 +13,3 @@ array_map(
         'post/init',
     ]
 );
-
-add_filter('timber/context', 'add_to_context');
-
-function add_to_context($context)
-{
-    // So here you are adding data to Timber's context object, i.e...
-    // Now, in similar fashion, you add a Timber Menu and send it along to the context.
-    $context['mobileSubmenu'] = new \Timber\Menu('mobile-sub-menu');
-    
-    return $context;
-}
--- app/loaders/menus.php
+++ app/loaders/menus.php
@@ -1,5 +1,15 @@
 <?php
 
+add_filter('timber/context', 'add_to_context');
+function add_to_context($context)
+{
+	// So here you are adding data to Timber's context object, i.e...
+	// Now, in similar fashion, you add a Timber Menu and send it along to the context.
+	$context['mobileSubmenu'] = new \Timber\Menu('mobile-sub-menu');
+	
+	return $context;
+}
+
 function cdv_register_nav_menu()
 {
     register_nav_menus(

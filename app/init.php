23a9e7e356e814ef102914033a4bfe1e676385dc
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-02-04 08:27:38 +0100

    added mobile detecting lib
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- app/init.php
+++ app/init.php
@@ -13,3 +13,12 @@ array_map(
         'post/init',
     ]
 );
+
+
+add_filter('timber/context', 'add_to_context');
+
+function add_to_context($context)
+{
+    $context['device'] = new Mobile_Detect();
+    return $context;
+}

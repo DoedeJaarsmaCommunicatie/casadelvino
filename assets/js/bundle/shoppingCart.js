fe4b824dec9b5deb307f3538702c991bc05a1187
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-02-04 09:30:03 +0100

    update wc ajax cart
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- assets/js/bundle/shoppingCart.js
+++ assets/js/bundle/shoppingCart.js
@@ -1,18 +1,18 @@
 /* eslint-disable no-undef */
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('.wac-btn-sub').forEach((el) => {
-    el.addEventListener('click', () => {
-      document.querySelector('button.update-cart').click();
-    });
-  });
-  document.querySelectorAll('.wac-btn-inc').forEach((el) => {
-    el.addEventListener('click', () => {
-      document.querySelector('button.update-cart').click();
-    });
-  });
-  document.querySelectorAll('.qty').forEach((el) => {
-    el.addEventListener('change', () => {
-      document.querySelector('button.update-cart').click();
-    });
-  });
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//   document.querySelectorAll('.wac-btn-sub').forEach((el) => {
+//     el.addEventListener('click', () => {
+//       document.querySelector('button.update-cart').click();
+//     });
+//   });
+//   document.querySelectorAll('.wac-btn-inc').forEach((el) => {
+//     el.addEventListener('click', () => {
+//       document.querySelector('button.update-cart').click();
+//     });
+//   });
+//   document.querySelectorAll('.qty').forEach((el) => {
+//     el.addEventListener('change', () => {
+//       document.querySelector('button.update-cart').click();
+//     });
+//   });
+// });

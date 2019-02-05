7f6959cdecee9300248a8bf5477d24414b967e15
Mitch Hijlkema <mitchhijlkema@hotmail.com>
2019-02-05 15:25:13 +0100

    adding button to hide filter
    Signed-off-by: Mitch Hijlkema <mitchhijlkema@hotmail.com>

--- woocommerce/myaccount/my-address.php
+++ woocommerce/myaccount/my-address.php
@@ -16,21 +16,21 @@
  * @version 2.6.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+if (! defined('ABSPATH')) {
+    exit; // Exit if accessed directly
 }
 
 $customer_id = get_current_user_id();
 
-if ( ! wc_ship_to_billing_address_only() && wc_shipping_enabled() ) {
-	$get_addresses = apply_filters( 'woocommerce_my_account_get_addresses', array(
-		'billing' => __( 'Billing address', 'woocommerce' ),
-		'shipping' => __( 'Shipping address', 'woocommerce' ),
-	), $customer_id );
+if (! wc_ship_to_billing_address_only() && wc_shipping_enabled()) {
+    $get_addresses = apply_filters('woocommerce_my_account_get_addresses', array(
+        'billing' => __('Billing address', 'woocommerce'),
+        'shipping' => __('Shipping address', 'woocommerce'),
+    ), $customer_id);
 } else {
-	$get_addresses = apply_filters( 'woocommerce_my_account_get_addresses', array(
-		'billing' => __( 'Billing address', 'woocommerce' ),
-	), $customer_id );
+    $get_addresses = apply_filters('woocommerce_my_account_get_addresses', array(
+        'billing' => __('Billing address', 'woocommerce'),
+    ), $customer_id);
 }
 
 $oldcol = 1;
@@ -38,28 +38,28 @@ $col    = 1;
 ?>
 
 <p>
-	<?php echo apply_filters( 'woocommerce_my_account_my_address_description', __( 'The following addresses will be used on the checkout page by default.', 'woocommerce' ) ); ?>
+	<?php echo apply_filters('woocommerce_my_account_my_address_description', __('The following addresses will be used on the checkout page by default.', 'woocommerce')); ?>
 </p>
 
-<?php if ( ! wc_ship_to_billing_address_only() && wc_shipping_enabled() ) : ?>
-	<div class="u-columns woocommerce-Addresses col2-set addresses">
+<?php if (! wc_ship_to_billing_address_only() && wc_shipping_enabled()) : ?>
+	<div class="u-columns woocommerce-Addresses col2-set addresses row">
 <?php endif; ?>
 
-<?php foreach ( $get_addresses as $name => $title ) : ?>
+<?php foreach ($get_addresses as $name => $title) : ?>
 
-	<div class="u-column<?php echo ( ( $col = $col * -1 ) < 0 ) ? 1 : 2; ?> col-<?php echo ( ( $oldcol = $oldcol * -1 ) < 0 ) ? 6 : 6; ?> woocommerce-Address">
+	<div class="u-column<?php echo (($col = $col * -1) < 0) ? 1 : 2; ?> col-<?php echo (($oldcol = $oldcol * -1) < 0) ? 6 : 6; ?> woocommerce-Address">
 		<header class="woocommerce-Address-title title">
 			<h3><?php echo $title; ?></h3>
-			<a href="<?php echo esc_url( wc_get_endpoint_url( 'edit-address', $name ) ); ?>" class="edit"><?php _e( 'Edit', 'woocommerce' ); ?></a>
+			<a href="<?php echo esc_url(wc_get_endpoint_url('edit-address', $name)); ?>" class="edit"><?php _e('Edit', 'woocommerce'); ?></a>
 		</header>
 		<address><?php
-			$address = wc_get_account_formatted_address( $name );
-			echo $address ? wp_kses_post( $address ) : esc_html_e( 'You have not set up this type of address yet.', 'woocommerce' );
-		?></address>
+            $address = wc_get_account_formatted_address($name);
+            echo $address ? wp_kses_post($address) : esc_html_e('You have not set up this type of address yet.', 'woocommerce');
+        ?></address>
 	</div>
 
 <?php endforeach; ?>
 
-<?php if ( ! wc_ship_to_billing_address_only() && wc_shipping_enabled() ) : ?>
+<?php if (! wc_ship_to_billing_address_only() && wc_shipping_enabled()) : ?>
 	</div>
 <?php endif;
